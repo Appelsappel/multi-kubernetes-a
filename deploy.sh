3836b0c4ee4ddd0023553db1147000c2a37027e3
Ruben Keur <r.keur@qquest.nl>
2020-03-13 13:53:09 +0100

    added / to ./client etc

--- deploy.sh
+++ deploy.sh
@@ -1,6 +1,6 @@
-docker build -t appelsappel/multi-client-a:latest -t appelsappel/multi-client-a:$SHA -f ./client/Dockerfile .client
-docker build -t appelsappel/multi-api-a:latest -t appelsappel/multi-api-a:$SHA -f ./api/Dockerfile .api
-docker build -t appelsappel/multi-worker-a:latest -t appelsappel/multi-worker-a:$SHA -f ./worker/Dockerfile .worker
+docker build -t appelsappel/multi-client-a:latest -t appelsappel/multi-client-a:$SHA -f ./client/Dockerfile ./client
+docker build -t appelsappel/multi-api-a:latest -t appelsappel/multi-api-a:$SHA -f ./api/Dockerfile ./api
+docker build -t appelsappel/multi-worker-a:latest -t appelsappel/multi-worker-a:$SHA -f ./worker/Dockerfile ./worker
 
 docker push appelsappel/multi-client-a:latest
 docker push appelsappel/multi-api-a:latest
